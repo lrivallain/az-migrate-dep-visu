486eb1e42de8284a5b2f30164bb68bf8c088a558
Ludovic Rivallain <ludovic.rivallain@gmail.com>
2025-01-17 19:16:53 +0100

    Remove unused TF content

--- Terraform/provider.tf
+++
@@ -1,9 +0,0 @@
-provider "azurerm" {
-  features {}
-  subscription_id = var.subscription_id
-  resource_providers_to_register = [
-    "Microsoft.Containers",
-    "Microsoft.Containerservice",
-    "Microsoft.App"
-  ]
-}
